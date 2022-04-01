9dde872fc5e605c32af231cc3cb0f520b86166b4
kuczmmar <atram888@gmail.com>
2022-04-01 16:34:30 +0100

    Compiled and added susan benchmark

--- gem5/run.sh
+++ gem5/run.sh
@@ -19,7 +19,14 @@ fi
 date
 
 # define benchmark variables
-BENCH_PATH='../benchmarks/cgo2017/program/randacc/bin/x86/randacc-no'
+RANDACC='../benchmarks/cgo2017/program/randacc/bin/x86/randacc-no'
+
+SUSAN='../benchmarks/cbench/automotive_susan_c/src/a.out'
+SUSAN_ARG="../benchmarks/cbench/automotive_susan_data/1.pgm,output_large.corners.pgm,-c"
+
+
+
+
 # define paths to configuration files
 O3_TWO_LEVEL='configs/runahead/o3_2level.py'
 GEM='build/X86/gem5.opt'
@@ -41,13 +48,15 @@ run_base() {
   ARG=$1
   L2=$2
   ROB=$3
-  RANDACC="--binary=${BENCH_PATH} --binary_args ${ARG}"
-  ARG="$((ARG/1000))k"
-  NAME="rob${ROB}_${ARG}_${L2}"
+  BENCH_NAME=$4
+  BENCH_PATH=$5
+  PROGRAM="--binary=${BENCH_PATH} --binary_args ${ARG}"
+  NAME="${BENCH_NAME}_rob${ROB}_${L2}"
+
   BASE_GEM_FLAGS="--stats-file=base/${NAME} --json-config=base/${NAME}_config.json ${BASE_DEBUG}"
   OUT="out/base_${NAME}.txt"
 
-  time $GEM $BASE_GEM_FLAGS $O3_TWO_LEVEL --mode=baseline --rob_size=$ROB --l2_size=$L2  $RANDACC > $OUT
+  time $GEM $BASE_GEM_FLAGS $O3_TWO_LEVEL --mode=baseline --rob_size=$ROB --l2_size=$L2  $PROGRAM > $OUT
 }
 
 
@@ -55,13 +64,15 @@ run_run() {
   ARG=$1
   L2=$2
   ROB=$3
-  RANDACC="--binary=${BENCH_PATH} --binary_args ${ARG}"
-  ARG="$((ARG/1000))k"
-  NAME="rob${ROB}_${ARG}_${L2}"
+  BENCH_NAME=$4
+  BENCH_PATH=$5
+  PROGRAM="--binary=${BENCH_PATH} --binary_args ${ARG}"
+  NAME="${BENCH_NAME}_rob${ROB}_${L2}"
+
   RA_GEM_FLAGS="--stats-file=run/${NAME} --json-config=run/${NAME}_config.json ${RUN_DEBUG}"
   OUT="out/run_${NAME}.txt"
 
-  time $GEM $RA_GEM_FLAGS  $O3_TWO_LEVEL --mode=runahead   --rob_size=$ROB  --l2_size=$L2  $RANDACC > $OUT
+  time $GEM $RA_GEM_FLAGS  $O3_TWO_LEVEL --mode=runahead   --rob_size=$ROB  --l2_size=$L2  $PROGRAM > $OUT
 }
 
 
@@ -69,19 +80,31 @@ run_pre() {
   ARG=$1
   L2=$2
   ROB=$3
-  RANDACC="--binary=${BENCH_PATH} --binary_args ${ARG}"
-  ARG="$((ARG/1000))k"
-  NAME="rob${ROB}_${ARG}_${L2}"
+  BENCH_NAME=$4
+  BENCH_PATH=$5
+  PROGRAM="--binary=${BENCH_PATH} --binary_args ${ARG}"
+  NAME="${BENCH_NAME}_rob${ROB}_${L2}"
+  
   PRE_GEM_FLAGS="--stats-file=pre/${NAME} --json-config=pre/${NAME}_config.json ${PRE_DEBUG}"
   OUT="out/pre_${NAME}.txt"
 
-  time $GEM $PRE_GEM_FLAGS  $O3_TWO_LEVEL --mode=pre      --rob_size=$ROB --l2_size=$L2 $RANDACC > $OUT
+  time $GEM $PRE_GEM_FLAGS  $O3_TWO_LEVEL --mode=pre      --rob_size=$ROB --l2_size=$L2 $PROGRAM > $OUT
 }
 
+
 run_all() {
-  run_base $1 $2 $3 &\
-  run_run  $1 $2 $3 &\
-  run_pre  $1 $2 $3 
+  run_base "$@" &  run_run  "$@" &  run_pre  "$@"
+}
+
+
+run_all_randacc() {
+  echo "Randomaccess run all: arg: ${1}, L2: ${2}, ROB: ${3}"
+  echo
+  ARG=$1
+  BNAME="randacc$((ARG/1000))k"
+  run_base "$@" $BNAME $RANDACC &
+  run_run  "$@" $BNAME $RANDACC &
+  run_pre  "$@" $BNAME $RANDACC
 }
 
 
@@ -91,29 +114,40 @@ run_all() {
 mkdir -p out m5out/base m5out/run m5out/pre
 echo_lines
 
-run_all  100000 '64kB' 128  &
-run_pre  525000 '256kB' 128  &  # crash
-run_pre  525000 '256kB' 64   &  # crash
-run_all  525000 '128kB' 192  &   # crash
-run_all  1000000 '256kB' 192 &
-run_pre  525000 '128kB' 192   &   # runs
-run_pre  525000 '256kB' 192  & # runs
-run_all  525000 '256kB' 192 &
-run_all  500000 '256kB' 192 &
-run_all  500000 '128kB' 192 &
+# run_all  100000 '64kB' 128  &
+# run_pre  525000 '256kB' 128  &  # crash
+# run_pre  525000 '256kB' 64   &  # crash
+# run_all  525000 '128kB' 192  &   # crash
+# run_all  1000000 '256kB' 192 &
+# run_pre  525000 '128kB' 192   &   # runs
+# run_pre  525000 '256kB' 192  & # runs
+# run_all  525000 '256kB' 192 &
+# run_all  500000 '256kB' 192 &
 
+run_all_randacc   500000      '64kB' 128 &
+run_all_randacc   600000      '64kB' 128 &
+run_all           $SUSAN_ARG  '64kB' 128 "susan" $SUSAN &
+run_all_randacc   500000      '128kB' 128 &
+run_all_randacc   600000      '128kB' 128 &
+run_all           $SUSAN_ARG  '128kB' 128 "susan" $SUSAN &
+run_all_randacc   500000      '256kB' 192 &
+run_all_randacc   600000      '256kB' 192 &
+run_all           $SUSAN_ARG  '256kB' 192 "susan" $SUSAN
 
 # runs fine:
-run_all  100000 '128kB' 128 &
-run_all  100000 '128kB' 128 &
-run_all  100000 '128kB' 64
+# run_all  100000 '128kB' 128 &
+# run_all  100000 '128kB' 128 &
+# run_all  100000 '128kB' 64
+
 # run_pre  100000 '64kB' 64 &
 # run_all  50000 '128kB' 128 & 
 # run_pre  50000 '64kB' 128 & 
 # run_all  60000 '128kB' 128 &
 # run_pre  60000 '64kB' 128
 
-# --l1i_size='32kB' --l1d_size='64kB' --l2_size='128kB'
+# run_all_randacc 60000 '64kB' 128
+
+
 wait
 wait
 wait
