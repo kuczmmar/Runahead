7c27131a04797b378ef40f2371921af2b1c8e0e4
kuczmmar <atram888@gmail.com>
2022-04-27 13:39:31 +0100

    Modifying cgo benchmarks

--- benchmarks/cgo2017/program/nas-cg/npbparams.h
+++ benchmarks/cgo2017/program/nas-cg/npbparams.h
@@ -4,10 +4,11 @@ c  This file is generated automatically by the setparams utility.
 c  It sets the number of processors and the class of the NPB
 c  in this directory. Do not modify it by hand.
 */
-#define	NA	75000
-#define	NONZER	13
-#define	NITER	75
-#define	SHIFT	60.0
+
+#define	NA	1400
+#define	NONZER	7
+#define	NITER	15
+#define	SHIFT	10.0
 #define	RCOND	1.0e-1
 #define	CONVERTDOUBLE	FALSE
 #define COMPILETIME "16 Aug 2016"
